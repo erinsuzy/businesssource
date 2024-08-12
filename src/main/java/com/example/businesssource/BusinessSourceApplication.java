c183cb7ee3b26ff7a0da67cbfaf307fe13ff9293
Erin Schneider <erinsschneider@gmail.com>
2024-08-12 11:35:39 -0500

    first commit

---
+++ src/main/java/com/example/businesssource/BusinessSourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.businesssource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/businesssource/BusinessSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businesssource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
