e725edb812a4629f5ccd3ded2940d46097b57f2a
Erin Schneider <erinsschneider@gmail.com>
2024-09-03 11:01:30 -0500

    user entity, repo, service added

---
+++ src/main/java/com/example/businesssource/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.businesssource.services;
+
+import com.example.businesssource.entities.User;
+import com.example.businesssource.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
+
